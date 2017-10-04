5637d64c003d432b809e27b29616ab4d8a02d773
Giandomenico Olini <giando.olini@gogodigital.it>
2017-10-04 16:50:49 +0200

    Add index to Menu Item migration

--- migrations/m160103_115802_create_menu_items.php
+++ migrations/m160103_115802_create_menu_items.php
@@ -32,8 +32,17 @@ class m160103_115802_create_menu_items extends Migration
             'language' => $this->string(7)->notNull()->defaultValue('all'),
         ]);
 
-        // Add Foreign Key
-        $this->addForeignKey("fk_menu_type", '{{%menu_items}}', "menutypeid", '{{%menu_types}}', "id", "CASCADE", "RESTRICT");
+        // Add Index and Foreign Key access
+        $this->createIndex(
+            "index_menu_type",
+            "{{%menu_items}}",
+            "menutypeid"
+        );
+
+        $this->addForeignKey("fk_menu_type",
+            '{{%menu_items}}', "menutypeid",
+            '{{%menu_types}}', "id",
+            "CASCADE", "RESTRICT");
 
         // Add Menu Item Root
         $this->insert('{{%menu_items}}', [
@@ -49,11 +58,9 @@ class m160103_115802_create_menu_items extends Migration
         $this->insert('{{%menu_items}}', [ 'id' => 2, 'menutypeid' => 1, 'title' => 'Home', 'alias' => 'home', 'link' => '/', 'parentid' => 1, 'state' => 1 ]);
         $this->insert('{{%menu_items}}', [ 'id' => 3, 'menutypeid' => 1, 'title' => 'About', 'alias' => 'about', 'link' => '/site/about', 'parentid' => 1, 'state' => 1 ]);
         $this->insert('{{%menu_items}}', [ 'id' => 4, 'menutypeid' => 1, 'title' => 'Contact', 'alias' => 'contact', 'link' => '/site/contact', 'parentid' => 1, 'state' => 1 ]);
-        $this->insert('{{%menu_items}}', [ 'id' => 5, 'menutypeid' => 1, 'title' => 'Category 1', 'alias' => 'category-1', 'link' => '/articles/categories/view',  'params' => '[{"id":"1","alias":"category-1"}]', 'parentid' => 1, 'state' => 1 ]);
-        $this->insert('{{%menu_items}}', [ 'id' => 6, 'menutypeid' => 1, 'title' => 'Test 1', 'alias' => 'test-1', 'link' => '/articles/items/view', 'params' => '[{"id":"1","alias":"test-1","cat":"category-1"}]','parentid' => 1, 'state' => 1 ]);
-        $this->insert('{{%menu_items}}', [ 'id' => 7, 'menutypeid' => 1, 'title' => 'Register', 'alias' => 'register', 'link' => '/register', 'access' => 'only guest', 'parentid' => 1, 'state' => 1 ]);
-        $this->insert('{{%menu_items}}', [ 'id' => 8, 'menutypeid' => 1, 'title' => 'Login', 'alias' => 'login', 'link' => '/login', 'access' => 'only guest', 'parentid' => 1, 'state' => 1 ]);
-        $this->insert('{{%menu_items}}', [ 'id' => 9, 'menutypeid' => 1, 'title' => 'Logout', 'alias' => 'logout', 'link' => '/logout', 'access' => 'registered', 'linkOptions' => '[{"data-method":"post"}]', 'parentid' => 1, 'state' => 1 ]);
+        $this->insert('{{%menu_items}}', [ 'id' => 5, 'menutypeid' => 1, 'title' => 'Register', 'alias' => 'register', 'link' => '/register', 'access' => 'only guest', 'parentid' => 1, 'state' => 1 ]);
+        $this->insert('{{%menu_items}}', [ 'id' => 6, 'menutypeid' => 1, 'title' => 'Login', 'alias' => 'login', 'link' => '/login', 'access' => 'only guest', 'parentid' => 1, 'state' => 1 ]);
+        $this->insert('{{%menu_items}}', [ 'id' => 7, 'menutypeid' => 1, 'title' => 'Logout', 'alias' => 'logout', 'link' => '/logout', 'access' => 'registered', 'linkOptions' => '[{"data-method":"post"}]', 'parentid' => 1, 'state' => 1 ]);
     }
 
     public function down()
